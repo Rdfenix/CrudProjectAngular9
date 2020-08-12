b2191bd8e07d17d721222feb6c71ef7e67114744
Rudnei Carlos de Oliveira/INTELLIGENT SOLUTIONS/S2/Samsung Electronics <rudnei.o@samsung.com>
2020-08-12 16:17:05 -0300

    change the version of rxjs

--- ProductApp/src/app/product/product.component.ts
+++ ProductApp/src/app/product/product.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from './../service/product.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-
-  constructor() { }
+  constructor(private productsService: ProductService) {}
 
   ngOnInit(): void {
+    this.productsService.get().subscribe((prods) => console.log(prods));
   }
-
 }
--- ProductApp/src/app/service/product.service.ts
+++ ProductApp/src/app/service/product.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { BehaviorSubject, Observable, combineLatest } from 'rxjs';
+import { Product } from '../interface/product';
+import { DepartmentService } from './department.service';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  readonly url = `${environment.url}/products`;
+  private productsSubject$: BehaviorSubject<Product[]> = new BehaviorSubject<
+    Product[]
+  >(null);
+  private loaded: boolean = false;
+  constructor(
+    private http: HttpClient,
+    private departmentService: DepartmentService
+  ) {}
 
-  constructor() { }
+  get(): Observable<Product[]> {
+    if (!this.loaded) {
+      combineLatest(
+        this.http.get<Product[]>(this.url),
+        this.departmentService.get()
+      )
+        .pipe(
+          map(([products, departments]) => {
+            for (let prod of products) {
+              const ids = prod.departments as string[];
+              prod.departments = ids.map((id) =>
+                departments.find((dep) => dep._id == id)
+              );
+            }
+            return products;
+          })
+        )
+        .subscribe(this.productsSubject$);
+
+      this.loaded = true;
+      // this.http.get<Product[]>(this.url).subscribe(this.productsSubject$);
+    }
+    return this.productsSubject$.asObservable();
+  }
+
+  // add(): Observable<Product> {}
+
+  // update(): Observable<Product> {}
 }
